4a621a6022edb5e98b2338fc0b74e498ae54e21d
Ayberk Levendoglu <ayberklevendoglu@gmail.com>
2023-09-12 23:25:25 +0300

    styling

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const navLinks = document.querySelectorAll(".nav-link");
 const dropdown = document.querySelector(".dropdown-menu");
+const navBar = document.querySelector(".nav-bar");
 navLinks.forEach((link) => {
   link.addEventListener("mouseover", () => {
     dropdown.classList.add("show");
+    navBar.style.opacity = 1;
   });
   link.addEventListener("mouseleave", () => {
     dropdown.classList.remove("show");
+    navBar.style.opacity = 0.8;
   });
 });
